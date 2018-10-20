19a1ebc0232a56037894477fde2bb86612cf3b94
Josh Cash <master_cash@live.com>
2018-10-20 18:20:50 -0500

    Set up an Empty Plugin Shell.

---
+++ src/main/java/io/github/mastercash/timber/Main.java
@@ -0,0 +1,15 @@
+package io.github.mastercash.timber;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
