7dccec764fc4df2654901035efed4e76555cd710
Josh Cash <master_cash@live.com>
2018-10-21 14:41:43 -0500

    forgot to setup event in main. Main now registers the log breaking event. also updated the gitignore to ignore the .idea folder

--- src/main/java/io/github/mastercash/timber/Main.java
+++ src/main/java/io/github/mastercash/timber/Main.java
@@ -1,11 +1,12 @@
 package io.github.mastercash.timber;
 
+import io.github.mastercash.timber.events.LogBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        getServer().getPluginManager().registerEvents(new LogBreakEvent(), this);
     }
 
     @Override
